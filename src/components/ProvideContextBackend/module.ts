239325a2df55c02e50ceaf3ab44cda6010de9128
Christopher Díaz <nealthmc@gmail.com>
2022-01-01 16:23:36 -0500

    [ADD] nonce on header request

--- src/components/ProvideContextBackend/module.ts
+++ src/components/ProvideContextBackend/module.ts
@@ -48,33 +48,42 @@ function requester(stage: Stage<State>, method: BackendRequestMethodsAllowed): R
   }) => {
     const { state } = stage
     const { endpoint, params, updateCache } = args
-
+  
     const searchParams = (new URLSearchParams(params)).toString()
     const urlParams = `${endpoint}${searchParams ? `?${searchParams}` : ''}`
     const id = `${method}:${urlParams}`
     const [inQueue] = state.queueCallbacks?.filter(queueCallback => queueCallback.id === id) ?? []
 
     if ((updateCache || !state.store?.get(id)) && state.queueCallbacks && !inQueue) {
-      const requestInit: RequestInit = {}
-      requestInit.credentials = 'include'
-      requestInit.method = method
-      requestInit.headers = {
-        Authorization: `Bearer ${extractCookie('access_token')}`,
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'X-CSRF-TOKEN': `${stage.state.store?.get(`GET:${resources.endpoints.get.userCsrf}`)?.data}`,
-      }
-  
-      if (method !== 'GET') {
-        requestInit.body = JSON.stringify(params)
-      }
-  
       const path = `${resources.path.backendUrlBase}${method === 'GET' ? urlParams : endpoint}`
-      const callback = () => fetch(path, requestInit)
+      const callback = requestCallback(path, stage, method, params)
       stage.state.queueCallbacks?.push({ callback, method, endpoint, params, id })
       stage.commitState({})
     }
+  }
+}
 
+function requestCallback(
+  path: string,
+  stage: Stage<State>,
+  method: BackendRequestMethodsAllowed,
+  params?: Record<string, string>,
+) {
+  return () => {
+    const options: RequestInit = {}
+    if (method !== 'GET') {
+      options.body = JSON.stringify(params)
+    }
+    options.credentials = 'include'
+    options.method = method
+    options.headers = {
+      Authorization: `Bearer ${extractCookie('access_token')}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRF-TOKEN': `${stage.state.store?.get(`GET:${resources.endpoints.get.userCsrf}`)?.data}`,
+      'nonce': `${~~(new Date().getTime() / 1000)}`
+    }
+    return fetch(path, options)
   }
 }
 
