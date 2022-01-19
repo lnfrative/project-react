f9a28a53603ecf2bc6b8cd64b1177ad85f98e558
Christopher Díaz <nealthmc@gmail.com>
2022-01-19 02:43:19 +0100

    [ADD] CheckboxRhomboidTerms on storybook

---
+++ src/components/CheckboxRhomboidTerms/index.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ComponentStory } from '@storybook/react'
+import { CheckboxRhomboidTermsProps } from 'interfaces'
+import { CheckboxRhomboidTerms } from 'components'
+
+export default {
+  title: 'CheckboxRhomboidTerms',
+  component: CheckboxRhomboidTerms,
+}
+
+const Template: ComponentStory<
+  typeof CheckboxRhomboidTerms
+> = (props: CheckboxRhomboidTermsProps) => (
+  <CheckboxRhomboidTerms {...props} />
+)
+
+export const Test = Template.bind({})
+
+Test.args = {
+  checkboxRhomboidProps: {
+    onCheck: () => {}
+  }
+}
