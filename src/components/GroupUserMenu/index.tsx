8552ec5a85beddf5ae470879f7393346e56dd228
Christopher Díaz <nealthmc@gmail.com>
2022-01-22 08:24:24 -0500

    [ADD] user character on menu

--- src/components/GroupUserMenu/index.tsx
+++ src/components/GroupUserMenu/index.tsx
@@ -1,19 +1,33 @@
 // region import
-import React from 'react'
+import React, { useContext } from 'react'
+
+// interfaces
+import { BackendUser } from 'interfaces'
 
 // components
 import { SVGIconNotifications, MenuOptionsUser } from 'components'
+
+// contexts
+import { Backend } from 'contexts'
+
+// utilities
+import { resources } from 'utilities'
 
 // styles
 import styles from './index.module.css'
 // endregion
 
 function GroupUserMenu() {
+	const { response } = useContext(Backend)
+	const user: BackendUser | undefined = response.get({
+		endpoint: resources.endpoints.get.user,
+	})?.data
+	const letter = user?.email.slice(0, 1).toUpperCase() ?? ''
 	return (
 		<div className={styles.container}>
 			<SVGIconNotifications />
 			<div className={styles.menu}>
-				<MenuOptionsUser character="W" />
+				<MenuOptionsUser character={letter} />
 			</div>
 		</div>
 	)
