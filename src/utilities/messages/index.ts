ebe50ecb3076ab3cd2abc4d6b7cc4bc4970f202a
Christopher Díaz <nealthmc@gmail.com>
2022-01-29 06:01:04 -0500

    [UPDATE] style on ModalBoxEnable2FA

--- src/utilities/messages/index.ts
+++ src/utilities/messages/index.ts
@@ -179,7 +179,7 @@ const messages: Record<string, Record<string, string>> = {
 		en: 'Enable',
 	},
 	ENABLE_TWO_FACTOR: {
-		en: 'Enable two-factor authentication',
+		en: 'Enable two-factor',
 	},
 	ENABLE_TWO_FACTOR_INFO: {
 		en: 'Scan this code using any authenticator app, such as the free "Google Authenticator" (Android and iOS). This software generates a time-based one-time password that you\'ll need to enter to authenticate yourself.',
