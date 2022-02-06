245074ca4fc0df47602ee7b4e60e12dd46333b91
Christopher Díaz <nealthmc@gmail.com>
2022-02-05 21:05:54 -0500

    [FIX] empty wallets overview

--- src/components/Overview/index.tsx
+++ src/components/Overview/index.tsx
@@ -91,7 +91,7 @@ function Overview() {
 					<div className={styles.groupTitle}>{message({ id: 'ASSETS_SUMMARY' })}</div>
 					<div className={styles.groupValues}>
 						<div className={styles.assetsTable}>
-							{wallets && coins && (
+							{wallets.length !== 0 && coins && (
 								<div className={styles.assetsTableRow}>
 									<div />
 									<div className={styles.headerTitle}>{message({ id: 'PRICE' })}</div>
