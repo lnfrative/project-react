e0d56ac801f35dffc96daf6c12abf130d2ce799c
Christopher <nealthmc@gmail.com>
2022-02-28 03:36:03 -0500

    [ADD] Income with secondary content

--- src/components/Income/index.tsx
+++ src/components/Income/index.tsx
@@ -8,7 +8,7 @@ import { Panel, ChartCurve, ValueDecimalLabel } from 'components'
 import { message } from 'utilities'
 
 // styles
-import { Container, StyledPanel, Revenues } from './style'
+import { Container, StyledPanel, Revenues, PrimaryContent, SecondaryContent } from './style'
 // endregion
 
 const weight = [600.0, 600.2, 610, 601.4, 509.9, 600.2, 509.8, 508.6, 509.6, 509.2, 602, 600]
@@ -18,22 +18,29 @@ const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', '
 function Income() {
 	return (
 		<Container>
-			<StyledPanel>
-				<Panel title={message({ id: 'REVENUE_SUMMARY' })}>
-					<Revenues>
-						<ValueDecimalLabel value={0} decimals={2} title="Today" sign="$" sise="large" />
-						<ValueDecimalLabel value={0} decimals={2} title="This week" sign="$" sise="large" />
-						<ValueDecimalLabel value={0} decimals={2} title="This month" sign="$" sise="large" />
-						<ValueDecimalLabel value={0} decimals={2} title="This year" sign="$" sise="large" />
-					</Revenues>
-				</Panel>
-			</StyledPanel>
+			<PrimaryContent>
+				<StyledPanel>
+					<Panel title={message({ id: 'REVENUE_SUMMARY' })}>
+						<Revenues>
+							<ValueDecimalLabel value={0} decimals={2} title="Today" sign="$" sise="large" />
+							<ValueDecimalLabel value={0} decimals={2} title="This week" sign="$" sise="large" />
+							<ValueDecimalLabel value={0} decimals={2} title="This month" sign="$" sise="large" />
+							<ValueDecimalLabel value={0} decimals={2} title="This year" sign="$" sise="large" />
+						</Revenues>
+					</Panel>
+				</StyledPanel>
 
-			<StyledPanel>
-				<Panel title={message({ id: 'REVENUE_PER_MONTH' })}>
-					<ChartCurve data={weight} labels={labels} />
-				</Panel>
-			</StyledPanel>
+				<StyledPanel>
+					<Panel title={message({ id: 'REVENUE_PER_MONTH' })}>
+						<ChartCurve data={weight} labels={labels} />
+					</Panel>
+				</StyledPanel>
+			</PrimaryContent>
+			<SecondaryContent>
+				<StyledPanel>
+					<Panel title="Income stats">test</Panel>
+				</StyledPanel>
+			</SecondaryContent>
 		</Container>
 	)
 }
--- src/components/Income/style.ts
+++ src/components/Income/style.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled('div')``
+export const Container = styled('div')`
+	display: flex;
+`
 
 export const StyledPanel = styled('div')`
 	margin-bottom: ${props => props.theme.space.lg};
@@ -10,4 +12,13 @@ export const Revenues = styled('div')`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
+`
+
+export const PrimaryContent = styled('div')`
+	flex: 5;
+`
+
+export const SecondaryContent = styled('div')`
+	flex: 2;
+	margin-left: ${props => props.theme.space.lg};
 `
