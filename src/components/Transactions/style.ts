a8818391c3393687453d08246273a5211b11e76a
Christopher <nealthmc@gmail.com>
2022-03-09 07:39:18 -0500

    [UPDATE] Filters first on Transactions mobile view

--- src/components/Transactions/style.ts
+++ src/components/Transactions/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled('div')`
 	display: flex;
-	flex-direction: column;
+	flex-direction: column-reverse;
 
 	${props => props.theme.mediaQuery.xl} {
 		flex-direction: row;
